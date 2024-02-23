ea4b45fc38681d950d0824b118a09f881f4acd38
varun kumar <varunkumar19999210@gmail.com>
2024-02-23 21:10:44 +0530

    21 linked list

---
+++ MergedTwoSortedArrays_21.java
@@ -0,0 +1,40 @@
+package Leetcode;
+
+public class MergedTwoSortedArrays_21 {
+
+	  public class ListNode {
+		     int val;
+		    ListNode next;
+		     ListNode() {}
+		      ListNode(int val) { this.val = val; }
+		     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+		  }
+		
+		class Solution {
+		    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		        ListNode Dummy = new ListNode();
+		        ListNode temp = Dummy;
+		        while(list1!=null && list2!=null){
+		            if(list1.val<list2.val){
+		                temp.next=list1;
+		                list1=list1.next;
+		                temp = temp.next;
+		            }
+		            else{
+		                temp.next=list2;
+		                list2=list2.next;
+		                temp = temp.next;
+		            }
+		        }
+		        if(list1==null){
+		            temp.next = list2;
+		        }
+		        if(list2==null){
+		            temp.next = list1;
+		        }
+		        return Dummy.next;
+		    }
+
+	}
+
+}
