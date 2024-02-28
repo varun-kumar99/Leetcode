d35c61ccc5c1a1950cc99a21c8beaab325e5df5a
varun kumar <varunkumar19999210@gmail.com>
2024-02-28 22:24:29 +0530

    34 array

---
+++ Find1stAndLastPositionofElement_34.java
@@ -0,0 +1,58 @@
+package Leetcode;
+import java.util.*;
+public class Find1stAndLastPositionofElement_34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n  = sc.nextInt();
+		int []A = new int[n];
+		for(int i =0;i<n;i++) {
+			A[i] = sc.nextInt();
+		}
+		int target = sc.nextInt();
+		searchRange(A,  target);
+		for(int i =0;i<A.length;i++) {
+			System.out.println(A[i]);
+		}
+		
+	}
+	 public static int[] searchRange(int[] nums, int target) {
+         int[] result = new int[2];
+    result[0] = findFirst(nums, target);
+    result[1] = findLast(nums, target);
+    return result;
+}
+
+private static int findFirst(int[] nums, int target){
+    int idx = -1;
+    int start = 0;
+    int end = nums.length - 1;
+    while(start <= end){
+        int mid = (start + end) / 2;
+        if(nums[mid] >= target){
+            end = mid - 1;
+        }else{
+            start = mid + 1;
+        }
+        if(nums[mid] == target) idx = mid;
+    }
+    return idx;
+}
+
+private static int findLast(int[] nums, int target){
+    int idx = -1;
+    int start = 0;
+    int end = nums.length - 1;
+    while(start <= end){
+        int mid = (start + end) / 2;
+        if(nums[mid] <= target){
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
+        if(nums[mid] == target) idx = mid;
+    }
+    return idx;
+    }
+}
