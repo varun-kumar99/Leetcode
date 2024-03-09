7a3cc849349c22a168e46320a8f591786965224e
varun kumar <varunkumar19999210@gmail.com>
2024-03-10 00:02:37 +0530

    49 list string

--- GroupAnagrams_49.java
+++ GroupAnagrams_49.java
@@ -4,7 +4,33 @@ public class GroupAnagrams_49 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
+	
+	 public String getSignature(String s) {
+	        int[] count = new int[26];
+	        for (char c : s.toCharArray()) {
+	            count[c - 'a']++;
+	        }
+
+	        StringBuilder sb = new StringBuilder();
+	        for (int i = 0; i < 26; i++) {
+	            if (count[i] != 0) {
+	                sb.append((char) ('a' + i)).append(count[i]);
+	            }
+	        }
+	        return sb.toString();
+	    }
+
+	    public List<List<String>> groupAnagrams(String[] strs) {
+	        List<List<String>> result = new ArrayList<>();
+	        Map<String, List<String>> groups = new HashMap<>();
+
+	        for (String s : strs) {
+	            groups.computeIfAbsent(getSignature(s), k -> new ArrayList<>()).add(s);
+	        }
+
+	        result.addAll(groups.values());
+
+	        return result;
 
 }
