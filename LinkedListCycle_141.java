58cd463194ca8cd12ac8fa12e5f657b3d182f9be
varun kumar <varunkumar19999210@gmail.com>
2024-05-08 22:25:51 +0530

    141 linked list

---
+++ LinkedListCycle_141.java
@@ -0,0 +1,10 @@
+package Leetcode;
+
+public class LinkedListCycle_141 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- WordBreak_139.java
+++ WordBreak_139.java
@@ -1,10 +1,38 @@
 package Leetcode;
-
+import java.util.*;
 public class WordBreak_139 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	  public boolean wordBreak(String s, List<String> wordDict) {
+
+	        Set<String> dict = new HashSet<>(wordDict);
+
+	        int n = s.length();
+
+	        boolean[] dp = new boolean[n + 1];
+
+	        dp[0] = true;
+
+	        for (int i = 1; i <= n; ++i) {
+
+	            for (int j = 0; j < i; ++j) {
+
+	                if (dp[j] && dict.contains(s.substring(j, i))) {
+
+	                    dp[i] = true;
+
+	                    break;
+
+	                }
+
+	            }
+
+	        }
+
+	        return dp[n];
+	  }
 
 }
