d7562085b49bd0b70b3dab8349672e5a70a620ab
varun kumar <varunkumar19999210@gmail.com>
2024-05-25 14:41:59 +0530

    array matrix bfs dfs

--- BinaryTreeRightSideView_199.java
+++ BinaryTreeRightSideView_199.java
@@ -1,10 +1,48 @@
 package Leetcode;
-
+import java.util.*;
 public class BinaryTreeRightSideView_199 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	public class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode() {}
+		      TreeNode(int val) { this.val = val; }
+		      TreeNode(int val, TreeNode left, TreeNode right) {
+		          this.val = val;
+		          this.left = left;
+		          this.right = right;
+		      }
+		  }
+		 
+		class Solution {
+		    int max_depth;
+		    public List<Integer> rightSideView(TreeNode root) {
+		        max_depth = -1;
+					List<Integer> ll = new ArrayList<>();
+					View(root, ll, 0);
+					return ll;
+
+				}
+
+				public void View(TreeNode root, List<Integer> ll, int curr_lev) {
+
+					if (root == null) {
+						return;
+					}
+					if (curr_lev > max_depth) {
+						ll.add(root.val);
+						max_depth = curr_lev;
+					}
+
+					View(root.right, ll, curr_lev + 1);
+					View(root.left, ll, curr_lev + 1);
+
+				}
+		}
 
 }
