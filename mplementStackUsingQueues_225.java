f4ba5895be5a32450a7e26e1fa11036aac7bdb5e
varun-kumar99 <varunkumar19999210@gmail.com>
2024-06-13 00:00:05 +0530

    228
    228 array

--- mplementStackUsingQueues_225.java
+++ mplementStackUsingQueues_225.java
@@ -1,10 +1,40 @@
 package Leetcode;
-
+import java.util.*;
 public class mplementStackUsingQueues_225 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	class MyStack {
+
+	    private Queue<Integer> q = new LinkedList<>();
+
+	    public void push(int x) {
+	        q.add(x);
+	    }
+
+	    public int pop() {
+	        int size = q.size();
+	        for (int i = 0; i < size - 1; i++) {
+	            push(q.remove());
+	        }
+	        return q.remove();
+	    }
+
+	    public int top() {
+	        int size = q.size();
+	        for (int i = 0; i < size - 1; i++) {
+	            push(q.remove());
+	        }
+	        int topValue = q.remove();
+	        push(topValue);
+	        return topValue;
+	    }
+
+	    public boolean empty() {
+	        return q.isEmpty();
+	    }
 	}
 
 }
